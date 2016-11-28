b5694fd32cef1a06b47be52cbb195dce664c35f2
sgnl <rayrfarias@gmail.com>
2016-11-27 19:46:55 -1000

    refactors a littlel defines promise lib for mongoose

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ const mongoose = require('mongoose');
 
 const CONFIG = require('./config/');
 
-// mongoose.connect(`mongodb://${CONFIG.MONGO.USER}:${CONFIG.MONGO.PASSWORD}@${CONFIG.MONGO.URL}`);
-got(`http://longs.staradvertiser.com/oahu/${process.argv[2]}/pdf/oahu${process.argv[2]}.pdf`)
-  .then(response => {
-    console.log('status code is: ', response.statusCode, ' \\o/ ');
-  })
+mongoose.connect(`mongodb://${CONFIG.MONGO.USER}:${CONFIG.MONGO.PASSWORD}@${CONFIG.MONGO.URL}`);
+mongoose.Promise = global.Promise;
+
+const Brochure = mongoose.model('Brochure', {url: String});
+
+const newBrochureURL = `http://longs.staradvertiser.com/oahu/${process.argv[2]}/pdf/oahu${process.argv[2]}.pdf`;
+
+function addNewBrochureToDatabase(response) {
+  if (response.statusCode === 200) {
+    const newBrochure = {url: newBrochureURL};
+
+    Brochure.findOneAndUpdate(newBrochure, newBrochure, {upsert: true, new: true})
+      .then(doc => {
+        console.log('success: ', doc);
+      });
+  }
+}
+
+// :sparkles:
+got(newBrochureURL)
+  .then(addNewBrochureToDatabase)
   .catch(err => {
     console.log(err.response.status);
   });
