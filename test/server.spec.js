25ed08cf9838e069eed6c11d49260e8f2ba0075d
sgnl <rayrfarias@gmail.com>
2017-02-01 22:20:20 -1000

    adds basic test

---
+++ test/server.spec.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+const test = require('ava');
+const req = require('supertest');
+// const pug = require('pug');
+
+const app = require('../server');
+
+test('', async t => {
+  let res = await req(app).get('/');
+  t.is(res.status, 200);
+  t.regex(res.headers['content-type'], /html/);
+  t.truthy(res.body);
+  // const templateHTML = pug.renderFile('./views/enroll.pug');
+  // t.is(res.text, templateHTML);
+});
