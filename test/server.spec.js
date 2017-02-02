9339021dffbf6b0161609801c0d900c095ea85aa
sgnl <rayrfarias@gmail.com>
2017-02-02 13:13:55 -1000

    🎨 moves HTML render test into own suite

--- test/server.spec.js
+++ test/server.spec.js
@@ -7,10 +7,14 @@ const pug = require('pug');
 
 const app = require('../server');
 
-test('GET `/` ', async t => {
-  let res = await req(app).get('/');
+test('GET `/` should return with 200 and serve HTML', async t => {
+  const res = await req(app).get('/');
   t.is(res.status, 200);
   t.regex(res.headers['content-type'], /html/);
+});
+
+test('GET `/` HTML should render with brochures', async t => {
+  const res = await req(app).get('/');
 
   const { findMostRecentUrls } = require('../services/mongo-service');
   const brochures = await findMostRecentUrls();
